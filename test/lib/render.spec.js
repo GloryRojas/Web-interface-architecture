c54b44cc4439d18cff8a52cc3c6eb8f0b1eef59d
gloryrojas <glory.rojas>
2021-03-12 18:26:09 -0500

    feat: adding unit test to render component

---
+++ test/lib/render.spec.js
@@ -0,0 +1,23 @@
+import render from '../../src/lib/render';
+
+describe('render', () => {
+  it('should render component in given target', () => {
+    const Component = () => document.createElement('div');
+    const target = document.createElement('div');
+    expect(render(Component, target)).toBeUndefined();
+    expect(target.children.length).toBe(1);
+    expect(target.children[0] instanceOf HTMLDivElement).toBe(true);
+  });
+  it('should empty target before rendering', () => {
+    const Component = () => document.createElement('div');
+    const target = document.createElement('div');
+    expect(render(Component, target)).toBeUndefined();
+    expect(target.children.length).toBe(1);
+    expect(target.children[0] instanceOf HTMLDivElement).toBe(true);
+  });
+  it('should not append when component returns falsy', () => {
+    const target = document.createElement('div');
+    expect(render(() => null, target)).toBeUndefined();
+    expect(target.children.length).toBe(0);
+  });
+});
